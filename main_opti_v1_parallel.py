caad2680e4b5fe652a06b59b27b721cb7f98726b
todortodor <lepot.simon@gmail.com>
2022-07-27 09:47:42 +0200

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit f0a9d7b4dafc02b7317b6a40b9c1d9026521ac9a, reversing
    changes made to cb05aed0516a79dbe34e63171039ceb388d2a18e.

--- main_opti_v1_parallel.py
+++ main_opti_v1_parallel.py
@@ -16,12 +16,10 @@ import numpy as np
 # from deco import *
 import time
 from time import perf_counter
-import multiprocessing as mp
 from multiprocessing import Pool
 from multiprocessing import Manager
-import concurrent.futures
 
-dir_num = 6
+dir_num = 5
 data_path = main_path+'data/'
 results_path = 'results/'
 
@@ -37,7 +35,7 @@ APTA = d.countries_from_fta('APTA')
 MERCOSUR = d.countries_from_fta('MERCOSUR')
 
 # carb_cost_list = np.append(np.linspace(0,2.5e-4,251),np.linspace(2.5e-4,1e-3,76)[1:])[46:]
-carb_cost_list = np.linspace(0,1e-4,21)
+carb_cost_list = np.linspace(0,1e-4,5)
 # eta_path = ['elasticities_agg1.csv','elasticities_agg2.csv','uniform_elasticities_4.csv']
 # sigma_path = ['elasticities_agg1.csv','elasticities_agg2.csv','uniform_elasticities_4.csv']
 eta_path = ['elasticities_agg1.csv']
@@ -58,6 +56,28 @@ cases = d.build_cases(eta_path,sigma_path,carb_cost_list,taxed_countries_list,ta
                       taxed_sectors_list,specific_taxing_list,fair_tax_list)
 
 years = [2018]
+
+# @concurrent
+# def work(baseline, data_path,results_path,dir_num, simulation_case):
+#     params = d.params(data_path, **simulation_case)
+#     params.num_scale_carb_cost(baseline.num, inplace = True)
+    
+#     if not params.fair_tax:
+#         results = s.solve_E_p(params, baseline)
+    
+#     if params.fair_tax:
+#         results = s.solve_fair_tax(params, baseline)
+    
+#     #compute some aggregated solution quantities to write directly in runs report
+#     emissions_sol, utility, utility_countries = s.compute_emissions_utility(results, params, baseline)
+    
+#     # d.write_solution_csv(results,results_path,dir_num,emissions_sol,utility,params,baseline)
+
+# @synchronized
+# def run(baseline, data_path,results_path,dir_num, cases):
+#     for i,simulation_case in enumerate(cases):
+#         work(baseline, data_path,results_path,dir_num, simulation_case)
+
     
 def one_run(data_tuple):
     baseline, data_path, results_path, dir_num, simulation_case = data_tuple
@@ -75,9 +95,10 @@ def one_run(data_tuple):
     emissions_sol, utility, utility_countries = s.compute_emissions_utility(results, params, baseline)
     
     d.write_solution_csv(results,results_path,dir_num,emissions_sol,utility,params,baseline)
-    # return simulation_case['carb_cost']
-     
-
+    return simulation_case['carb_cost']
+    
+# if __name__ == '__main__':    
+t1 = perf_counter()
 # for y in years:
 y=2018         
 year=str(y)
@@ -89,16 +110,18 @@ baseline.num_scale(numeraire_type, numeraire_country, inplace = True)
 baseline.make_np_arrays(inplace = True)
 
 baseline.compute_shares_and_gammas(inplace = True)
-    
-#%%
-t1 = perf_counter()
 
-# if __name__ == '__main__':   
-#     with concurrent.futures.ThreadPoolExecutor() as executor:
-#         executor.map(one_run, [(baseline, data_path, results_path, dir_num, simulation_case) for simulation_case in cases])
+# run(baseline, data_path,results_path,dir_num, cases)
 
+# p = Pool()
+# data = [(baseline, data_path, results_path, dir_num, simulation_case) for simulation_case in cases]
+# manager = Manager()
+# data = manager.list([(baseline, data_path, results_path, dir_num, simulation_case) for simulation_case in cases])
+# start = time.time()
+# for dat in p.map_async(one_run, data).get():
+#     print("{} (Time elapsed: {}s)".format(dat, int(time.time() - start)),flush=True)
+    # results = p.imap_unordered(one_run, data)
 for simulation_case in tqdm(cases):
-    
     
     params = d.params(data_path, **simulation_case)
     params.num_scale_carb_cost(baseline.num, inplace = True)
@@ -108,7 +131,10 @@ for simulation_case in tqdm(cases):
     
     if params.fair_tax:
         results = s.solve_fair_tax(params, baseline)
-    one_run((baseline, data_path, results_path, dir_num, simulation_case))
     
+    #compute some aggregated solution quantities to write directly in runs report
+    emissions_sol, utility, utility_countries = s.compute_emissions_utility(results, params, baseline)
+    
+    d.write_solution_csv(results,results_path,dir_num,emissions_sol,utility,params,baseline)
 t2 = perf_counter()
 print(t2-t1)
